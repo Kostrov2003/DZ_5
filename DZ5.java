d1150446cb20d5fce4cee704fcb7aa3ee921ea6e
Kostrov2003 <93771387+Kostrov2003@users.noreply.github.com>
2022-12-12 14:48:42 +0300

    Update DZ5.java

--- DZ5.java
+++ DZ5.java
@@ -2,11 +2,11 @@ public class Main
 {
 	public static void main(String[] args) {
 		Person[] persArray = new Person[5];
-        persArray[0] = new Person("Рогаченко Даниил", "инженер", "daniil.98@mailbox.com", 892312312, 30000, 30); 
-        persArray[1] = new Person("Салихов Салих", "программист", "salikh.2003@mail.com", 877777777, 50000, 18);
-        persArray[2] = new Person("Петров Петр", "заместитель директора", "petrov@yande.com", 879767472, 100000, 45);
-        persArray[3] = new Person("Базаров Евгений", "системный аналитик", "jenia@google.ru", 839745632, 80000, 41);
-        persArray[4] = new Person("Солопов Александр", "директор", "f.s.v@bing.ru", 880053475, 200000, 55);
+        persArray[0] = new Person("ВИТЯ ИВАНОВ", "ЛОХ", "DFD@MAIL.RU",890101010101, 545845, 30); 
+        persArray[1] = new Person("ПЕТЯ ПЕТРОВ", "ЖИВ", "PETYCOOL@MAIL.RU", 888888, 44545, 18);
+        persArray[2] = new Person("ЖОРИК ПОРИК", "ЦЕЛ", "JORAPORA@MAIL.RU", 000000000, 17575, 45);
+        persArray[3] = new Person("МАРГАРИТА АРМЯНКА", "ОРЕЛ", "RITA500R1CH@MAIL.RU", 89775674554, 12554, 41);
+        persArray[4] = new Person("ИЛЬНАЗ ДА", "DEADEND", "PROTASHERGGGGGG@MGOTU.GG", 88005553535, 999999999999999999999999, 22);
         for (int i = 0; i < persArray.length; i++){
             persArray[i].age();
 	}
